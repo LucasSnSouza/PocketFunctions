cc9160be5215456de97382c231e813a9e36f9f23
Lucas Dos Santos Souza <lucassantospublico@gmailc.om>
2025-11-10 18:37:44 -0300

    add / new content for enviroment sounds

--- src/components/Sound/Basic.vue
+++ src/components/Sound/Basic.vue
@@ -1,5 +1,78 @@
 <template>
+
+    <div class="sound-basic-wrapper shadow-sm w-full p-lg rounded-md bg-color-brand-one color-brand-three ghost flex x-start y-center gap-lg">
+
+        <ButtonBasic
+            class="rounded-md p-lg bg-color-brand-three"
+        >
+            <MiscIcon
+                class="bg-color-brand-one"
+                icon="play-fill"
+                :size="[20,20]"
+            />
+        </ButtonBasic>
+
+        <div class="sound-basic-information color-brand-two flex flex-column gap-md w-full">
+            <h1 class="font-md">Chuva leve</h1>
+            <input class="sound-basic-slide" type="range" min="1" max="100"/>
+        </div>
+
+    </div>
+
 </template>
 
 <script>
+
+import * as Misc from "@/components/Misc"
+import * as Button from "@/components/Button"
+
+export default {
+    data(){
+        return{
+        }
+    },
+    components: {
+        ...Misc,
+        ...Button,
+    },
+    methods: {
+    },
+    computed: {
+    },
+    created(){
+    }
+}
+
+
 </script>
+
+<style lang="scss">
+
+.sound-basic-wrapper{
+
+
+    .sound-basic-information{
+
+        .sound-basic-slide{
+            appearance: none;
+            height: 4px;
+            background: var(--color-brand-three);
+            border-radius: 2px;
+        }
+
+        .sound-basic-slide::-webkit-slider-thumb{
+            appearance: none;
+            width: 14px;
+            height: 14px;
+            border: 3px solid var(--color-brand-three);
+            background: white;
+            border-radius: var(--scale-brand-sm);
+            box-shadow: 3px 3px 4px rgba(0, 0, 0, 0.150);
+            cursor: pointer;
+        }
+
+    }
+
+}
+
+</style>
--- src/views/EnvironmentView.vue
+++ src/views/EnvironmentView.vue
@@ -1,13 +1,40 @@
 <template>
 
-    <div class="wrapper-homeview h-full font-sm">
+    <div class="wrapper-homeview h-full font-sm flex flex-column gap-md">
 
-        <ButtonBasic
-            class="rounded-md p-lg w-full bg-color-brand-three"
-        >
-            <p class="color-brand-two">Adicionar novo Som</p>
-        </ButtonBasic>
+        <h1 class="font-md">Sons Ambientes</h1>
+        <p class="w-3-4">
+            Faça do seu dispositivo um gerador de sons ambientes confortaveis para todas as horas.
+        </p>
 
+        <div>
+            <ButtonBasic
+                class="rounded-md p-lg bg-color-brand-three"
+            >
+                <MiscIcon
+                    class="bg-color-brand-one"
+                    icon="music-fill"
+                    :size="[20,20]"
+                />
+            </ButtonBasic>
+        </div>
+
+        <MiscDivision/>
+
+        <div class="flex flex-column gap-md">
+
+            <SoundBasic>
+            </SoundBasic>
+
+        </div>
+
+        <div class="flex x-center">
+            <MiscIcon
+                class="bg-color-brand-three"
+                icon="dots-fill"
+                :size="[24,24]"
+            />
+        </div>
 
     </div>
 
@@ -17,6 +44,7 @@
 
 import * as Misc from "@/components/Misc"
 import * as Button from "@/components/Button"
+import * as Sound from "@/components/Sound"
 
 export default {
     data(){
@@ -25,7 +53,8 @@ export default {
     },
     components: {
         ...Misc,
-        ...Button
+        ...Button,
+        ...Sound
     },
     methods: {
     },
